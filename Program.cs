08d7b52843384497737f4a0c9356dd79b72a1347
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-08 18:33:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Camekan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
