2e9a016b351ef6499727f54586caf5aac4d07e22
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-09 10:38:47 +0300

    creating delegate

---
+++ Camekan.DataAccess/Specification/BaseSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Camekan.DataAccess.Specification
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
