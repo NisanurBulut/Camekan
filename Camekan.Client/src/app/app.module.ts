74c84e638871188da0e533d54d245ba7883c7e56
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-26 00:08:50 +0300

    created navbar component

--- Camekan.Client/src/app/app.module.ts
+++ Camekan.Client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
