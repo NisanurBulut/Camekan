b7ca37b70e29d5c26bd3118fca05643e15ccf01f
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-27 20:08:08 +0300

    product child item component created and prepared  basic trailer

--- Camekan.Client/src/app/shop/shop.module.ts
+++ Camekan.Client/src/app/shop/shop.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ShopService } from './shop.service';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, ProductItemComponent],
   imports: [
     CommonModule
   ],
