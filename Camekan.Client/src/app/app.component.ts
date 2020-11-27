1c31a58c0e46dcb640ecbf772c4188e3810fb2d2
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-27 15:20:12 +0300

    listed products from api basically on the app component

--- Camekan.Client/src/app/app.component.ts
+++ Camekan.Client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Camekan';
+  products: any[];
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:63484/api/product?pageSize=50')
+      .subscribe((response: any) => {
+       this.products=response.data;
+       console.log(this.products);
+
+      }, error => {
+        console.log(error);
+      });
+  }
 }
--- Camekan.Client/src/app/app.module.ts
+++ Camekan.Client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
