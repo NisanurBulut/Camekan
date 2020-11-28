09ab7ab45db7a2982bf5ac4d31d0613bb3613a01
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-28 17:39:55 +0300

    created oage-header component and use it in shared module

--- Camekan.Client/src/app/shared/shared.module.ts
+++ Camekan.Client/src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagingHeaderComponent } from './components/paging-header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [PagingHeaderComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
   exports: [
-    PaginationModule
+    PaginationModule,
+    PagingHeaderComponent
   ]
 })
 export class SharedModule { }
