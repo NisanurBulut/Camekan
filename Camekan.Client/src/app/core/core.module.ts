3d09d9f30a467b8dc177b468b834d7a74d06e31c
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-28 23:18:08 +0300

    created error component for 500 and 404 codes

--- Camekan.Client/src/app/core/core.module.ts
+++ Camekan.Client/src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule
