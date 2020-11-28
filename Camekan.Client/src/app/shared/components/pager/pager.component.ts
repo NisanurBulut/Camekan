d35acd85c1d1dc6899fb32ca32395355341ff906
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-28 17:52:43 +0300

    implemented pager component

---
+++ Camekan.Client/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent implements OnInit {
+  @Input() pageSize: number;
+  @Input() totalCount: number;
+  @Output() pageChanged = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onPageChanged(event: any) {
+    this.pageChanged.emit(event);
+  }
+}
--- Camekan.Client/src/app/shared/shared.module.ts
+++ Camekan.Client/src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { PagingHeaderComponent } from './components/paging-header.component';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent],
+  declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot()
   ],
   exports: [
     PaginationModule,
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagerComponent
   ]
 })
 export class SharedModule { }
