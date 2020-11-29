94da27b4d061b20f00321fcbc474d99e0d6d0f4a
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-29 23:42:14 +0300

    implemented context seed class

---
+++ Camekan.DataAccess/Context/DatabaseIdentityContextSeed.cs
@@ -0,0 +1,48 @@
+﻿using Camekan.Entities;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+
+namespace Camekan.DataAccess.Context
+{
+    public class DatabaseIdentityContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager,ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                
+                if (!userManager.Users.Any())
+                {
+                    var user = new AppUser
+                    {
+                        DisplayName="Nisanur",
+                        Email="nisanurbulutnb@gmail.com",
+                        UserName="nisanurrunasin",
+                        Address=new Address
+                        {
+                            City="Eskişehir",
+                            State="ESES",
+                            Street="Adalar",
+                            ZipCode="2600"
+                        }
+                    };
+                    await userManager.CreateAsync(user,"1");
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<DatabaseContextSeed>();
+                logger.LogError(ex, "An error occured during seeding data");
+            }
+        }
+    }
+}
