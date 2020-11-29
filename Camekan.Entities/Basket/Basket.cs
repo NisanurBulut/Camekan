b09b8a41c938189323f01b1f321d92af8fe0d654
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-29 16:35:13 +0300

    implemented basket and basketItem entities

---
+++ Camekan.Entities/Basket/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Camekan.Entities
+{
+   public class Basket
+    {
+        public Basket()
+        {
+
+        }
+        public Basket(string id)
+        {
+            Id = id;
+        }
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+    }
+}
