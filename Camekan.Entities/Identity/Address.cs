928671699002c1b93935c722fd8d887061780382
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-29 23:14:01 +0300

    implemented identity entity classes

---
+++ Camekan.Entities/Identity/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Camekan.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        [Required]
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
