63dad5f078ad273eab276b908e258413fb545382
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-29 16:55:06 +0300

    implemented basketcontroller

---
+++ Camekan.API/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+﻿using Camekan.DataAccess.Repositories;
+using Camekan.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Camekan.WebAPI.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<BasketEntity>> GetBasketById(string id)
+        {
+            var basket = await _basketRepository.GetBasketAsync(id);
+            return Ok(basket?? new BasketEntity(id));
+        }
+        [HttpPost]
+        public async Task<ActionResult<BasketEntity>> UpdateBasket(BasketEntity basket)
+        {
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            return Ok(updatedBasket);
+        }
+        [HttpDelete]
+        public async Task DeleteBasket(string id)
+        {
+             await _basketRepository.DeleteBasketAsync(id);
+            
+        }
+    }
+}
