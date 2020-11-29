ba5d95554891988634fbc5eedf4cd594af56b28b
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-29 19:27:46 +0300

    implemented adding item to basket

--- Camekan.Client/src/app/basket/basket.service.ts
+++ Camekan.Client/src/app/basket/basket.service.ts
@@ -3,12 +3,15 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { IBasket } from '../shared/models/basket.model';
+import { Basket, IBasket } from '../shared/models/basket.model';
+import { IBasketItem } from '../shared/models/basketItem.model';
+import { IProduct } from '../shared/models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+
   baseUrl = environment.apiUrl;
   private basketSource = new BehaviorSubject<IBasket>(null);
   basket$ = this.basketSource.asObservable();
@@ -28,7 +31,43 @@ export class BasketService {
         this.basketSource.next(response);
       }, error => console.log(error));
   }
+
   getCurrenctBasketValue() {
     return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1) {
+    const itemToAdd: IBasketItem = this.mapProductToBasketItem(item, quantity);
+    const basket = this.getCurrenctBasketValue() ?? this.createBaasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+  }
+  addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
+    const index = items.findIndex(a => a.id === itemToAdd.id);
+    if (index === -1) {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
+      items[index].quantity += quantity;
+    }
+    return items;
+  }
+
+  private createBaasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private mapProductToBasketItem(item: IProduct, quantity: number): IBasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType
+    };
   }
 }
