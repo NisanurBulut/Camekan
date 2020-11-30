b4006858d99fbd9f1a551ef179c1f67617fbd738
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-30 19:16:48 +0300

    implemented account service

--- Camekan.Client/src/app/account/account.service.ts
+++ Camekan.Client/src/app/account/account.service.ts
@@ -1,9 +1,50 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AsyncSubject, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+  private currenUserSource = new BehaviorSubject<IUser>(null);
+  currentUser$ = this.currenUserSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(values: any) {
+    return this.http.post(this.baseUrl + '/account/login', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currenUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.http.post(this.baseUrl + '/account/register', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currenUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currenUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+  checkEmailExists(email: string) {
+    return this.http.get(this.baseUrl + '/account/emailexists?email=' + email);
+  }
 }
