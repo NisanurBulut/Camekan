0eed8998c8024fa01cced6074d1a20d8182af6dc
NisanurBulut <nisanurrunasin@gmail.com>
2020-11-30 18:25:01 +0300

    adding token control into swagger

--- Camekan.API/Extensions/SwaggerServiceExtension.cs
+++ Camekan.API/Extensions/SwaggerServiceExtension.cs
@@ -7,10 +7,25 @@ namespace Camekan.WebAPI.Extensions
     {
         public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
         {
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Camekan API", Version = "v1" });
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Token",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securityScheme);
+                var securityRequirement = new OpenApiSecurityRequirement { { securityScheme, new[] { "Bearer" } } };
+                c.AddSecurityRequirement(securityRequirement);
             });
             return services;
         }
