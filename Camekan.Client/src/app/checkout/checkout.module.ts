be883b0432d70c6af1c1842fe6c4c30fa607cdd1
NisanurBulut <nisanurrunasin@gmail.com>
2020-12-02 22:02:04 +0300

    implemented checkout page with order total

--- Camekan.Client/src/app/checkout/checkout.module.ts
+++ Camekan.Client/src/app/checkout/checkout.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CheckoutComponent],
   imports: [
     CommonModule,
-    CheckoutRoutingModule
+    CheckoutRoutingModule,
+    SharedModule
   ]
 })
 export class CheckoutModule { }
