4154de80e5e6155a6fde16d317fc1533994d2aaa
NisanurBulut <nbulut@sarplojistik.com>
2020-12-03 21:16:32 +0300

    implemented basket summary component

--- Camekan.Client/src/app/shared/components/basket-summary/basket-summary.component.ts
+++ Camekan.Client/src/app/shared/components/basket-summary/basket-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from '../../models/basket.model';
@@ -14,6 +14,7 @@ export class BasketSummaryComponent implements OnInit {
   @Output() decrement: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
   @Output() increment: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
   @Output() remove: EventEmitter<IBasketItem> = new EventEmitter<IBasketItem>();
+  @Input() isBasket: true;
 
   constructor(private basketService: BasketService) { }
 
