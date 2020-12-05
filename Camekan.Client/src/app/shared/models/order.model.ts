50aaa7f5192d5629869801119d8150cb324310d6
NisanurBulut <nbulut@sarplojistik.com>
2020-12-05 13:36:53 +0300

    implemented order model ts

---
+++ Camekan.Client/src/app/shared/models/order.model.ts
@@ -0,0 +1,27 @@
+import { IAddress } from './address.model';
+
+export interface IOrderToCreate {
+  basketId: string;
+  deliveryMethodId: number;
+  shipToAddress: IAddress;
+}
+export interface IOrder {
+  id: number;
+  buyerEmail: string;
+  orderDate: string;
+  shipToAddress: IAddress;
+  deliveryMethod: string;
+  shippingPrice: number;
+  orderItems: IOrderItem[];
+  subtotal: number;
+  total: number;
+  status: string;
+}
+
+export interface IOrderItem {
+  productId: number;
+  productName: string;
+  pictureUrl: string;
+  price: number;
+  quantity: number;
+}
