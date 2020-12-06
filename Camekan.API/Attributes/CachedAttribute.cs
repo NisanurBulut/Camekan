e832198fc47955df568f1970952dea35ecda36a7
NisanurBulut <nbulut@sarplojistik.com>
2020-12-06 18:03:45 +0300

    Creating a cache attribute class

---
+++ Camekan.API/Attributes/CachedAttribute.cs
@@ -0,0 +1,55 @@
+﻿using Camekan.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camekan.API.Helpers
+{
+    public class CachedAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly int _timeToLiveSeconds;
+        public CachedAttribute(int timeToLiveSeconds)
+        {
+            _timeToLiveSeconds = timeToLiveSeconds;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var responseService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+            var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+            var cachedResponse = await responseService.GetCachedResponseAsync(cacheKey);
+            if (!string.IsNullOrEmpty(cachedResponse))
+            {
+                var contentResult = new ContentResult
+                {
+                    Content = cachedResponse,
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+                context.Result = contentResult;
+                return;
+            }
+            var executedContent = await next(); // movw to controller
+            if (executedContent.Result is OkObjectResult okObject)
+            {
+                await responseService.CacheResponseAsync(cacheKey, okObject.Value, TimeSpan.FromSeconds(_timeToLiveSeconds));
+            }
+        }
+
+        private string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder();
+            keyBuilder.Append($"{request.Path}");
+
+            foreach (var (key, value) in request.Query.OrderBy(x => x.Key))
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+            return keyBuilder.ToString();
+        }
+    }
+}
