bd587ac3219d8c417c760b15ba9023bfdbf992aa
NisanurBulut <nbulut@sarplojistik.com>
2020-12-06 16:07:21 +0300

    implemented webhook for stripe

--- Camekan.API/Controllers/PaymentController.cs
+++ Camekan.API/Controllers/PaymentController.cs
@@ -3,6 +3,9 @@ using Camekan.Entities;
 using Camekan.Util.Errors;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Stripe;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Camekan.WebAPI.Controllers
@@ -12,8 +15,10 @@ namespace Camekan.WebAPI.Controllers
     public class PaymentController : BaseApiController
     {
         private readonly IPaymentService _paymentService;
+        private readonly ILogger _logger;
+        private const string WhSecret = "";
 
-        public PaymentController(IPaymentService paymentService)
+        public PaymentController(IPaymentService paymentService, ILogger<IPaymentService> logger)
         {
             this._paymentService = paymentService;
         }
@@ -27,6 +32,30 @@ namespace Camekan.WebAPI.Controllers
             if (basket == null) return BadRequest(new ApiResponse(400,"Sepet bilgilerine erişilemedi"));
 
             return basket;
+        }
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ActionResult> StripeWebHook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
+
+            PaymentIntent paymentIntent;
+            OrderEntity orderEntity;
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    paymentIntent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment succceded : " , paymentIntent.Id);
+                    break;
+                case "payment_intent.failed":
+                    paymentIntent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment failed : ", paymentIntent.Id);
+                    break;
+            }
+            return new EmptyResult();
         }
     }
 }
