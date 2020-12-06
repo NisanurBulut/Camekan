1bf4c0e0267f213091898eee10bc40cff7719ec2
NisanurBulut <nbulut@sarplojistik.com>
2020-12-06 15:03:13 +0300

    implemented spinner into payment intent id control async event

--- Camekan.Client/src/app/core/interceptors/loading.interceptor.ts
+++ Camekan.Client/src/app/core/interceptors/loading.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { ThrowStmt } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { delay, finalize } from 'rxjs/operators';
+import { delay, finalize, retryWhen } from 'rxjs/operators';
 import { BusyService } from '../services/busy.service';
 
 @Injectable()
@@ -10,9 +10,13 @@ export class LoadingInterceptor implements HttpInterceptor {
 
     constructor(private busyService: BusyService) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (!req.url.includes('emailexists')) {
-            this.busyService.busy();
+        if (req.method === 'POST' && req.url.includes('order')) {
+            return next.handle(req);
         }
+        if (!req.url.includes('emailexists')) {
+            return next.handle(req);
+        }
+        this.busyService.busy();
         return next.handle(req).pipe(
             delay(1000),
             finalize(() => this.busyService.idle())
