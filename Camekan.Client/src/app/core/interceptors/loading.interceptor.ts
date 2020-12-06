6e6fee54a21b1a6983d06378ae12a6da8d407ef8
NisanurBulut <nbulut@sarplojistik.com>
2020-12-06 19:19:50 +0300

    styled shop module

--- Camekan.Client/src/app/core/interceptors/loading.interceptor.ts
+++ Camekan.Client/src/app/core/interceptors/loading.interceptor.ts
@@ -13,6 +13,9 @@ export class LoadingInterceptor implements HttpInterceptor {
         if (req.method === 'POST' && req.url.includes('order')) {
             return next.handle(req);
         }
+        if (req.method === 'DELETE') {
+            return next.handle(req);
+        }
         if (!req.url.includes('emailexists')) {
             return next.handle(req);
         }
